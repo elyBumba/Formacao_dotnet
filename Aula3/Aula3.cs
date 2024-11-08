c2f49c47b582ae107db70834eceb07fd1b1afe5d
Elisangela Tomas Custodio (DBI) <elisangela.custodio@atlantico.ao>
2024-11-08 18:36:23 +0100

    Add a new Lessons

---
+++ Aula3/Aula3.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Aula03{
+    static void Main()
+    { 
+        byte n1 = 10;
+        int num=10;
+        int num1=5;
+        //char letra='8';
+        //float valor =5.3f;
+        string nome="Bruno";
+
+        var aux=nome;
+        Console.WriteLine(nome);
+        Console.Write(nome + ":" + num + " mais " +num1);
+
+    }
+}
